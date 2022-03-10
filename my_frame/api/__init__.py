93b822f0bfe8fa7673ec8af066b059eff5ab1cd7
Troy Arnold <troy@Troys-MacBook-Pro.local>
2022-03-09 18:27:51 -0800

    Got API working to serve the filename

--- my_frame/api/__init__.py
+++ my_frame/api/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from my_frame.api.my_frame_api import HelloWorld
+from my_frame.api.my_frame_api import FetchPost
 
 
 # Initialize the API component.
@@ -9,4 +9,4 @@ api_blueprint = Blueprint("api", __name__)
 _api = Api(api_blueprint)
 
 # Register the resources of the API to it.
-_api.add_resource(HelloWorld, "/helloworld")
+_api.add_resource(FetchPost, '/<string:image_id>')
--- my_frame/api/my_frame_api.py
+++ my_frame/api/my_frame_api.py
@@ -1,6 +1,36 @@
-from flask_restful import Resource
+from flask_restful import Resource,reqparse, abort
+from my_frame.models import Image_Post
+
+image_ids = {}
+
+def abort_if_nil_id(image_id):
+    if image_id not in image_ids:
+        abort(404, message='Item is unavailable.')
+
+def abort_if_id_exists(image_id):
+    if image_id in image_ids:
+        abort(409, message="Already exists")
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+image_id_args = reqparse.RequestParser()
+image_id_args.add_argument("image_post_id", type=int, help="Image Post ID is required.", required=True)
+
+class FetchPost(Resource):
+    def get(self, image_id):
+        result = Image_Post.query.filter_by(id=image_id)
+        for item in result:
+            return item.image
+
+    def put(self, image_id):
+        abort_if_id_exists(image_id)
+        args = image_id_args.parse_args()
+        image_ids[image_id] = args
+        return image_ids
+
+    def delete(self,image_id):
+        abort_if_nil_id(image_id)
+        del image_ids[image_id]
+        return "", 204
+
+
+
