3b4b3d2ad32098e32e5370632f369b2071166e8c
Troy Arnold <troy@Troys-MacBook-Pro.local>
2022-03-15 19:52:38 -0700

    Allow heic image file extensions on create images

--- my_frame/posts/forms.py
+++ my_frame/posts/forms.py
@@ -5,7 +5,7 @@ from flask_wtf.file import FileAllowed
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    picture = FileField('Upload Image', validators=[FileAllowed(['jpg','png','HEIF'])])
+    picture = FileField('Upload Image', validators=[FileAllowed(['jpg','png','HEIF', 'jpeg'])])
     submit = SubmitField('Submit')
 
 class SetActiveForm(FlaskForm):
