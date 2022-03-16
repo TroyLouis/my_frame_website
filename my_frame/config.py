ffddad8c569644b10c4a81a938b998372aac8522
Troy Arnold <troy@Troys-MacBook-Pro.local>
2022-03-16 16:54:37 -0700

    Changed Config

--- my_frame/config.py
+++ my_frame/config.py
@@ -1,8 +1,10 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 class Config:
     SECRET_KEY = os.environ.get('MY_FRAME_SECRET_KEY') #set to environment export key etc
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') #set to environment
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') #set to environment
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = '587'
     MAIL_USE_TLS = True
